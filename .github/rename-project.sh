ae268f9d75d36f5b47d13822fe201560c6b1a890
LowK <davidkevinit@gmail.com>
2022-12-02 16:12:35 +0700

    Change GHA job

--- .github/rename-project.sh
+++ .github/rename-project.sh
@@ -4,14 +4,12 @@ do
     case "${flag}" in
         a) author=${OPTARG};;
         n) name=${OPTARG};;
-        u) urlname=${OPTARG};;
         d) description=${OPTARG};;
     esac
 done
 
 echo "Author: $author";
 echo "Project Name: $name";
-echo "Project URL name: $urlname";
 echo "Description: $description";
 
 echo "Renaming project..."
@@ -21,11 +19,13 @@ name_upper="$(echo $name|tr 'a-z' 'A-Z')"
 # for filename in $(find . -name "*.*")
 for filename in $(git ls-files)
 do
-    sed -i "s/author_name/$author/g" $filename
-    sed -i "s/project_urlname/$urlname/g" $filename
-    sed -i "s/project_description/$description/g" $filename
-    sed "s/PROJECT_NAME/$name_upper/g" $filename
-    sed -i "s/project_name/$name/g" $filename
+    if [[ $filename == *".github"* ]]; then
+        continue
+    fi
+    sed -i -- "s/author_name/$author/g" $filename
+    sed -i -- "s/project_description/$description/g" $filename
+    sed -- "s/PROJECT_NAME/$name_upper/g" $filename
+    sed -i -- "s/project_name/$name/g" $filename
     echo "Renamed $filename"
 done
 
